e3535a38474fb4a924c605891e9deb4997b574bf
Thora <thora.reynisdottir@gmail.com>
2025-06-20 16:50:24 +0000

    Changing Inv

--- SS25/Assets/Scripts/Inventory/InventoryUI.cs
+++ SS25/Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,88 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
-
-using UnityEngine.InputSystem;
-using NUnit.Framework;
-
-
 
 public class InventoryUI : MonoBehaviour
 {
-    public GameObject inventoryPanel;
+    [SerializeField]
+    private InventoryUI itemPrefab;
 
-    public Player player;
-
-    public List<SlotUI> slots = new List<SlotUI>();
-
-    private SlotUI selectedSlot;
-
-    void Start()
-    {
-        inventoryPanel.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (Keyboard.current.tabKey.wasPressedThisFrame)
-        {
-            ToggleInventory();
-        }
-    }
-
-    public void ToggleInventory()
-    {
-        if(!inventoryPanel.activeSelf)
-        {
-            inventoryPanel.SetActive(true);
-            Refresh();
-        }
-        else
-        {
-            inventoryPanel.SetActive(false);
-        }
-    }
-
-    public void Refresh()
-    {
-        if(slots.Count == player.inventory.slots.Count)
-        {
-            for (int i = 0; i < slots.Count; i++)
-            {
-                if(player.inventory.slots[i].type != CollectableType.NONE)
-                {
-                    slots[i].SetItem(player.inventory.slots[i]);
-                }
-                else
-                {
-                    slots[i].SetEmpty();
-                }
-            }
-        }
-    }
-
-    public void Remove(int slotID)
-    {
-        Collectable itemToDrop = GameManager.instance.itemManager.GetItemByType(
-            player.inventory.slots[slotID].type);
-
-        if (itemToDrop != null)
-        {
-            player.DropItem(itemToDrop);
-            player.inventory.Remove(slotID);
-            Refresh();
-        }
-    }
-
-    public void OnSlotClicked(SlotUI slot)
-    {
-        // Unhighlight previous
-        if (selectedSlot != null)
-            selectedSlot.Highlight(false);
-
-        // Highlight new
-        selectedSlot = slot;
-        selectedSlot.Highlight(true);
-    }
+    [SerializeField]
+    private RectTransform contentPanel;
 }
