cb42b3eabe7ced5d74d15d47abfc72129ee383cd
NoiGauta <noi.gautason@gmail.com>
2025-06-23 16:21:04 +0000

    Firepoint fyrir enemy, "Enemy" tag, Ranged enemy, melee enemy, projectile diamond

---
+++ SS25/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+        Debug.Log("Player takes " + damage + " damage! Current health: " + CurrentHealth);
+
+        if (CurrentHealth <= 5)
+        {
+                       Debug.Log("Players heals 50 health");
+            CurrentHealth += 50;
+        }
+        if (CurrentHealth <= 0)
+        {
+        
+        }
+    }
+}
+
