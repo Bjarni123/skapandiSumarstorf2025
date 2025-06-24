14dbf6bf63a71c730403560258d371fdf223f801
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2025-06-24 09:34:16 +0000

    Further fleshed out Map save/load (#7)

--- SS25/Assets/Scripts/Map/Save/MapFileMonster.cs
+++ SS25/Assets/Scripts/Map/Save/MapFileMonster.cs
@@ -1,4 +1,10 @@
+using Newtonsoft.Json;
+using UnityEngine;
+
 public class MapFileMonster
 {
-  
+    [JsonProperty("type")]
+    public MapFileMonsterType Type;
+    [JsonProperty("position")]
+    public Vector2 Position;
 }
--- SS25/Assets/Scripts/Map/Save/MapFilePlayer.cs
+++ SS25/Assets/Scripts/Map/Save/MapFilePlayer.cs
@@ -1,4 +1,8 @@
+using Newtonsoft.Json;
+using UnityEngine;
+
 public class MapFilePlayer
 {
-  
+    [JsonProperty("position")]
+    public Vector2 Position;
 }
