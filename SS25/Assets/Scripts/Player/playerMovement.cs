e99f9b7057164951cbba82ef5b424a646da9108a
Bjarni123 <bjarni04@gmail.com>
2025-06-25 11:01:43 +0000

    Player with animations added, and combo in the up direction when attacking

--- SS25/Assets/Scripts/Player/playerMovement.cs
+++ SS25/Assets/Scripts/Player/playerMovement.cs
@@ -1,36 +1,84 @@
- using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    public float moveSpeed = 5f;
-
+    public float speed = 5f;
     Rigidbody2D rb;
-    Vector2 moveInput;
 
-    void Awake()
+    private Vector2 input;
+
+    Animator anim;
+    private Vector2 lastMoveDirection;
+    private bool facingRight = true;
+
+    void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        // rb.freezeRotation = true;
     }
 
     void Update()
     {
+        ProcessMovementInputs();
+        Animate();
+        HandleFlip();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("AutoAttack"))
+        {
+            rb.linearVelocity = input * speed;
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+    
+    private void ProcessMovementInputs()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("AutoAttack"))
+        {
+            return;
+        }
+
         var kb = Keyboard.current;
         // read WASD or arrow keys
-        float x = (kb.aKey.isPressed || kb.leftArrowKey.isPressed) ? -1f
+        float moveX = (kb.aKey.isPressed || kb.leftArrowKey.isPressed) ? -1f
                 : (kb.dKey.isPressed || kb.rightArrowKey.isPressed) ? 1f
                 : 0f;
-        float y = (kb.sKey.isPressed || kb.downArrowKey.isPressed) ? -1f
+        float moveY = (kb.sKey.isPressed || kb.downArrowKey.isPressed) ? -1f
                 : (kb.wKey.isPressed || kb.upArrowKey.isPressed) ? 1f
                 : 0f;
 
-        moveInput = new Vector2(x, y).normalized;
+        input = new Vector2(moveX, moveY).normalized;
+
+        if (input != Vector2.zero)
+        {
+            lastMoveDirection = input;
+        }
+
     }
 
-    void FixedUpdate()
+    void Animate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        anim.SetFloat("MoveX", input.x);
+        anim.SetFloat("MoveY", input.y);
+        anim.SetFloat("MoveMagnitude", input.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
+    }
+
+    void HandleFlip()
+    {
+        if (input.x < 0 && facingRight || input.x > 0 && !facingRight)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+            facingRight = !facingRight;
+        }
     }
 }
