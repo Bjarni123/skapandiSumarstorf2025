b715a46ed60e7681ae1c68e3a246d1278ded26f9
NoiGauta <noi.gautason@gmail.com>
2025-06-25 14:40:40 +0000

    Enemies collision with player, projectiles fly through enemies (from enemy through enemy), movement on rangedenemy and added GiantEnemy (#16)

--- SS25/Assets/Scripts/Enemy/EnemyAttack.cs
+++ SS25/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -2,19 +2,14 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-
     public Transform player;
     public float AttackRange = 1.5f;
     public float AttackCooldown = 1f;
     public int AttackDamage = 1;
 
+    [HideInInspector] public bool IsAttacking = false;
+
     private float _lastAttackTime;
-
-    void Start()
-    {
-
-    }
-
 
     void Update()
     {
@@ -24,13 +19,14 @@ public class EnemyAttack : MonoBehaviour
 
         if (distance <= AttackRange && Time.time >= _lastAttackTime + AttackCooldown)
         {
-            Attack();
+            StartCoroutine(Attack());
             _lastAttackTime = Time.time;
         }
     }
-
-    void Attack()
+    System.Collections.IEnumerator Attack()
     {
+        IsAttacking = true;
+
         Debug.Log("Enemy attacks player for " + AttackDamage + " damage!");
 
         PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
@@ -38,7 +34,11 @@ public class EnemyAttack : MonoBehaviour
         {
             playerHealth.TakeDamage(AttackDamage);
         }
+        // Wait a short time before allowing movement again
+        yield return new WaitForSeconds(0.4f); // you can adjust this
+        IsAttacking = false;
     }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
