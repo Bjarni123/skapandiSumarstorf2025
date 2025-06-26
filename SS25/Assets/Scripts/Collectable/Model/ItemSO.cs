3baea84c52f5dcae23e07d5155284d16cbda721d
Thora <144377784+ThcraXR@users.noreply.github.com>
2025-06-26 17:32:45 +0000

    Thora (#19)
    inv system, drop still in progress and some issue with showing durability aka stats on items

---
+++ SS25/Assets/Scripts/Collectable/Model/ItemSO.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.Model
+{
+    [CreateAssetMenu]
+    public abstract class ItemSO : ScriptableObject
+    {
+        [field: SerializeField]
+        public bool IsStackable { get; set; }
+
+        public int ID => GetInstanceID();
+
+        [field: SerializeField]
+        public int MaxStackSize { get; set; } = 1;
+
+        [field: SerializeField]
+        public string Name { get; set; }
+
+        [field: SerializeField]
+        [TextArea]
+        public string Description { get; set; }
+
+        [field: SerializeField]
+        public Sprite ItemImage { get; set; }
+
+        [field: SerializeField]
+        public List<ItemParameter> DefaultParametersList { get; private set; }
+    }
+
+    [Serializable]
+    public struct ItemParameter : IEquatable<ItemParameter>
+    {
+        public ItemParameterSO itemParameter;
+        public float value;
+
+        public bool Equals(ItemParameter other)
+        {
+            return other.itemParameter == itemParameter;
+        }
+    }
+}
