202ee2c7f16443237363f2b1d8075ee8a2bf4aaa
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2025-06-27 14:08:16 +0000

    Example: Map Rendering (#23)

--- SS25/Assets/Scripts/Map/MapChunkController.cs
+++ SS25/Assets/Scripts/Map/MapChunkController.cs
@@ -2,9 +2,32 @@ using UnityEngine;
 
 public class MapChunkController : MonoBehaviour
 {
+    public Vector2Int Index { get; set; }
+
     public void Start()
     {
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                var tile = GameMap.GetTile(Index.x * 8 + x, Index.y * 8 + y);
+                var go = (tile) switch
+                {
+                    GameMapTile.Grass =>
+                        Instantiate(Resources.Load<GameObject>("Tiles/Grass"), transform),
+                    GameMapTile.Sand =>
+                        Instantiate(Resources.Load<GameObject>("Tiles/Sand"), transform),
+                    GameMapTile.Water =>
+                        Instantiate(Resources.Load<GameObject>("Tiles/Water"), transform),
+                    _ => Instantiate(Resources.Load<GameObject>("Tiles/None"), transform),
+                };
+                go.transform.localPosition = new Vector3(x, y, 0);
+            }
+        }
     }
 
-    public void OnChange() { }
+    public void OnChange()
+    {
+        Debug.LogWarning("TODO");
+    }
 }
